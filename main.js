457173ac1cef515b994469cd5c973843230345ee
sadia554466 <pue.khulna@gmail.com>
2023-10-03 16:11:28 +0600

    P2C-Laravel-Command

---
+++ main.js
@@ -0,0 +1,62 @@
+let editor = document.querySelector("#editor");
+
+ace.edit(editor, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+let controller1 = document.querySelector("#controller1");
+
+ace.edit(controller1, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+let controller2 = document.querySelector("#controller2");
+
+ace.edit(controller2, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+let controller3 = document.querySelector("#controller3");
+
+ace.edit(controller3, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+let controller4 = document.querySelector("#controller4");
+
+ace.edit(controller4, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+
+let command1 = document.querySelector("#command1");
+
+ace.edit(command1, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+let command2 = document.querySelector("#command2");
+
+ace.edit(command2, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+let command3 = document.querySelector("#command3");
+
+ace.edit(command3, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+let command4 = document.querySelector("#command4");
+
+ace.edit(command4, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+let command5 = document.querySelector("#command5");
+
+ace.edit(command5, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+
